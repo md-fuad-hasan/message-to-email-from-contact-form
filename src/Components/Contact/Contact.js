282a273b8a011620dd9a061cbbd20e03f929b175
Fuad Hasan <fuad.ice.ru@gmail.com>
2023-05-25 19:33:38 +0600

    Without Validation using emailjs

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 class Contact extends Component{
@@ -9,7 +9,7 @@ class Contact extends Component{
             name : '',
             email : '',
             message : '',
-            formValid: false
+            
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,27 +22,39 @@ class Contact extends Component{
         this.setState({
             [name]:value
         });
-
-        if(this.state.name){
-            this.setState({
-                formValid: true
-            })
-        }
-
-       
-        console.log(this.state);
     }
 
     handleSubmit(e){
         
+        const param = {
+            name: this.state.name,
+            email: this.state.email,
+            message: this.state.message
+        }
 
-        console.log(this.state);
+       
         this.setState({
             name:'',
             email:'',
             message: '',
             formValid: false
         })
+
+
+        const YOUR_SERVICE_ID = 'service_elmjryu';
+        const YOUR_TEMPLATE_ID = 'template_vww8kwb';
+        const YOUR_PUBLIC_KEY = 'jzwDQcOyp7YawL9y8';
+
+
+        emailjs.send(YOUR_SERVICE_ID,YOUR_TEMPLATE_ID,param,YOUR_PUBLIC_KEY)
+        .then(res=>{
+            console.log(res);
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+
+
         e.preventDefault();
 
     }
@@ -58,7 +70,7 @@ class Contact extends Component{
                         <input type="text" className="name" value={this.state.name} name="name" placeholder="Enter your Name" onChange={(e)=>this.handleChange(e)} />
                         <input type="email" className="email" value={this.state.email} name='email' placeholder="Enter your Email" onChange={(e)=>this.handleChange(e)} />
                         <textarea rows={4} className="message" value={this.state.message} name="message" placeholder="Your message" onChange={(e)=>this.handleChange(e)} />
-                        <button type="submit" disabled={!this.state.formValid} className="submit" onClick={e=>this.handleSubmit(e)}>Submit</button>
+                        <button type="submit"  className="submit" onClick={e=>this.handleSubmit(e)}>Send</button>
                     </form>
                 </div>
                 
